31f57a801620abb601ba74478d7099d0c8607dc7
Deni Purwanto <100355910+denipurwanto10@users.noreply.github.com>
2025-09-16 12:43:39 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portofolio/",
-  plugins: [react(), tailwindcss()],
+  base: "/portofolio/", // atau "/" kalau di root
+  plugins: [react()],
 })
